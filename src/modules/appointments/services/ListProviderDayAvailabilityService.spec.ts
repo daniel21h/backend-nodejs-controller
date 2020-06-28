5ca08f8fa55db6ffdc68c77b7a1f91ef615b795d
daniel21h <hesseldaniel21@gmail.com>
2020-06-28 11:21:49 -0300

    feat: excluding old times

--- src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -16,12 +16,16 @@ describe('ListProviders', () => {
   it('should be able to list the month availability from provider', async () => {
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2020, 5, 27, 8, 0, 0),
+      date: new Date(2020, 5, 27, 14, 0, 0),
     });
 
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2020, 5, 27, 10, 0, 0),
+      date: new Date(2020, 5, 27, 15, 0, 0),
+    });
+
+    jest.spyOn(Date, 'now').mockImplementation(() => {
+      return new Date(2020, 5, 27, 11).getTime();
     });
 
     const availability = await listProviderDayAvailability.execute({
@@ -35,9 +39,12 @@ describe('ListProviders', () => {
     expect(availability).toEqual(
       expect.arrayContaining([
         { hour: 8, available: false },
-        { hour: 9, available: true },
+        { hour: 9, available: false },
         { hour: 10, available: false },
-        { hour: 11, available: true },
+        { hour: 13, available: true },
+        { hour: 14, available: false },
+        { hour: 15, available: false },
+        { hour: 16, available: true },
       ]),
     );
   });
--- src/modules/appointments/services/ListProviderDayAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.ts
@@ -1,5 +1,5 @@
 import { injectable, inject } from 'tsyringe';
-import { getHours } from 'date-fns';
+import { getHours, isAfter } from 'date-fns';
 
 // import AppError from '@shared/errors/AppError';
 
@@ -52,9 +52,13 @@ class ListProviderDayAvailabilityService {
         appointment => getHours(appointment.date) === hour,
       );
 
+      // Excluding old times
+      const currentDate = new Date(Date.now());
+      const compareDate = new Date(year, month - 1, day, hour);
+
       return {
         hour,
-        available: !hasAppointmentInHour,
+        available: !hasAppointmentInHour && isAfter(compareDate, currentDate),
       };
     });
 
