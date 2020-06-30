140d06479e632858e9b61262633e854d04c7a794
daniel21h <hesseldaniel21@gmail.com>
2020-06-30 10:58:08 -0300

    feat: emails template

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -26,20 +26,28 @@ class SendForgotEmailPasswordService {
 
   public async execute({ email }: IRequest): Promise<void> {
     // Verificando existencia do user by email
-    const checkUserExists = await this.usersRepository.findByEmail(email);
+    const user = await this.usersRepository.findByEmail(email);
 
-    if (!checkUserExists) {
+    if (!user) {
       throw new AppError('User does not exists.');
     }
 
-    const { token } = await this.userTokensRepository.generate(
-      checkUserExists.id,
-    );
+    const { token } = await this.userTokensRepository.generate(user.id);
 
-    await this.mailProvider.sendMail(
-      email,
-      `Pedido de recuperação de senha recebido: ${token}`,
-    );
+    await this.mailProvider.sendMail({
+      to: {
+        name: user.name,
+        email: user.email,
+      },
+      subject: '[Lavou] Recuperação de senha',
+      templateData: {
+        template: 'Olá, {{name}}: {{token}}',
+        variables: {
+          name: user.name,
+          token,
+        },
+      },
+    });
   }
 }
 
