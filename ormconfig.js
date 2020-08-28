5c91ccea7b8c90e24c246b2de964d887428a78c1
daniel21h <hesseldaniel21@gmail.com>
2020-08-28 19:23:52 -0300

    configs for deploy test in heroku

---
+++ ormconfig.js
@@ -0,0 +1,29 @@
+console.log('process.env.DATABASE_URL :>> ', process.env.DATABASE_URL);
+console.log('process.env.MONGODB_URI :>> ', process.env.MONGODB_URI);
+
+module.exports = {
+  "name": "default",
+  "type": "postgres",
+  "url": process.env.DATABASE_URL,
+  "entities": [
+    "./dist/modules/**/infra/typeorm/entities/*.js"
+  ],
+  "migrations": [
+    "./dist/shared/infra/typeorm/migrations/*.js"
+  ],
+  "cli": {
+    "migrationsDir": "./src/shared/infra/typeorm/migrations"
+  }
+},
+{
+  "name": "mongo",
+  "type": "mongodb",
+  "url": process.env.MONGODB_URI,
+  "host": "localhost",
+  "port": 27017,
+  "database": "lavou",
+  "useUnifiedTopology": true,
+  "entities": [
+    "./dist/modules/**/infra/typeorm/schemas/*.js"
+  ]
+}
